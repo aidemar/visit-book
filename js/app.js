6ecdcee4d5571c636753fa78dbb09e3798ee5244
aidemar <aidemarmag@hotmail.com>
2018-02-15 13:05:14 -0600

    primer función

--- js/app.js
+++ js/app.js
@@ -1,2 +1,37 @@
-var textInput = $(".text-input");
-console.log(textInput);
+//Función para previsualización
+$(".text-input").keyup(showPreview);
+
+
+function showPreview() {
+  var $textInput = $(".text-input").val();
+  console.log($textInput);
+
+  if($($textInput).val().length > 0) {
+    $($preview).html($textInput);
+  }
+
+}
+
+
+
+
+
+
+
+//Función para pintar
+$("#add-text").click(paintText);
+var $textInput = $(".text-input");
+
+function paintText(text) {
+  var $text = $textInput.val();
+  // console.log($text);
+
+  var $commentContainer = $("<div />");
+  var $containerP = $("<p />");
+
+  $commentContainer.append($containerP);
+  $containerP.append($text);
+
+
+  $(".comment-area").prepend($commentContainer);
+}
